f21f8d4e58148ebae37ab5021dece48a9cb3d4bc
KateBlazhko <kateugnicheva@gmail.com>
2022-01-07 13:16:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    let nReverse = 0;
+    n = checkNegative(n);
+    while (n >= 1) {
+      let lastNumber = n % 10;
+      nReverse = (nReverse * 10) + lastNumber;
+      n = Math.trunc(n / 10);
+    }
+    return nReverse;
+  }
+
+  const checkNegative = (n) => {
+    if (n < 0) { n = Math.abs(n) }
+    return n;
+  }
